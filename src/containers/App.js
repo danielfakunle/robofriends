824d88a3f5e9c4b9be46df0845d900bd3dffea32
danielfakunle <tosinfakunle@yahoo.com>
2022-11-30 16:17:27 -0500

    Finished app

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import './App.css';
+import Scroll from "../components/Scroll";
+// state is an object that describes your application
+// App is a smart component because it has state
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
+  }
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => { return response.json(); })
+      .then(users => { this.setState({ robots: users }) });
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+  render() {
+    const {robots, searchField} = this.state
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    if (!robots.length) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="nice">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
