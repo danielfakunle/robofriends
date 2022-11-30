1af11addce988853519372bb3c82f719e26875b1
danielfakunle <tosinfakunle@yahoo.com>
2022-11-30 16:42:10 -0500

    Added ErrorBoundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import './App.css';
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 // state is an object that describes your application
 // App is a smart component because it has state
 class App extends Component {
@@ -22,7 +23,7 @@ class App extends Component {
     this.setState({ searchField: event.target.value })
   }
   render() {
-    const {robots, searchField} = this.state
+    const { robots, searchField } = this.state
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
@@ -34,7 +35,9 @@ class App extends Component {
           <h1 className="nice">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
